57eb8531fae2ee5c7d8dd0e6ab51d7725473e424
billy <billyliadphillips@gmail.com>
2018-11-13 13:42:39 +0000

    Added final activeBook Reducer

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export default function SelectBook(book) {
+  //selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property
+  return {
+      type: 'BOOK_SELECTED',
+      payload: book
+  };
+}
+
+//action is just a function
